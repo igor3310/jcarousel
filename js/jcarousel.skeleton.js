29291d4b1b550524bd9fe628d0d59024332d9371
igor3310 <apapto@yandex.com>
2021-01-01 18:58:46 +0300

    Загрузка

---
+++ js/jcarousel.skeleton.js
@@ -0,0 +1,59 @@
+(function($) {
+    $(function() {
+        /*
+        Carousel initialization
+        */
+        $('.jcarousel')
+            .jcarousel({
+				wrap: 'both',
+				list: '.jcarousel-list',
+				items: '.jcarousel-item'
+                // Options go here
+            });
+			
+
+        /*
+         Prev control initialization
+         */
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '-=1'
+            });
+
+        /*
+         Next control initialization
+         */
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '+=1'
+            });
+
+        /*
+         Pagination initialization
+         */
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination({
+                // Options go here
+            });
+    });
+})(jQuery);
